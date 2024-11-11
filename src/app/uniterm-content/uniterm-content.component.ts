52b7787a3d2193134631c951fdbe4cc7283ab93a
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-11-11 22:01:03 +0100

    feat: create uniterm content component

---
+++ src/app/uniterm-content/uniterm-content.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-uniterm-content',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './uniterm-content.component.html',
+  styleUrl: './uniterm-content.component.scss',
+})
+export class UnitermContentComponent {
+  @Input() isVertical = true;
+
+  @Input() value = '';
+
+  getChars() {
+    return this.value.split('');
+  }
+
+  getUnitermClass() {
+    return this.isVertical ? 'uniterm--vertical' : 'uniterm--horizontal';
+  }
+}
--- src/app/uniterm/uniterm.component.ts
+++ src/app/uniterm/uniterm.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { UnitermContentComponent } from '../uniterm-content/uniterm-content.component';
 
 @Component({
   selector: 'app-uniterm',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UnitermContentComponent],
   templateUrl: './uniterm.component.html',
   styleUrl: './uniterm.component.scss',
 })
 export class UnitermComponent {
   @Input() isVertical = true;
+
+  @Input() left = '';
+
+  @Input() right = '';
 
   getUnitermClass() {
     return this.isVertical ? 'uniterm--vertical' : 'uniterm--horizontal';
