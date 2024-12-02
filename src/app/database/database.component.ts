a49ba656a41a5c806b289882f9a2247e39cf1298
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-12-02 20:36:10 +0100

    refactor: the app component

--- src/app/database/database.component.ts
+++ src/app/database/database.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-database',
-  imports: [],
   templateUrl: './database.component.html',
-  styleUrl: './database.component.scss'
+  styleUrl: './database.component.scss',
 })
-export class DatabaseComponent {
-
-}
+export class DatabaseComponent {}
