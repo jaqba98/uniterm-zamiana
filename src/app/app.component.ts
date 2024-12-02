6cd4a6b93a65c2498f94e53413a1b3a698b6c62e
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-12-02 20:18:32 +0100

    refactor: app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 import { FormComponent } from './form/form.component';
 import { PlaygroundComponent } from './playground/playground.component';
+import { DatabaseComponent } from './database/database.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [FormComponent, PlaygroundComponent],
+  imports: [FormComponent, PlaygroundComponent, DatabaseComponent],
 })
 export class AppComponent {}
